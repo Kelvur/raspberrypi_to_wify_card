3ac06e2df91efe21af9bdbbdfd329bba48a68cc7
Kelvur <Osorio10.92@gmail.com>
2017-08-09 18:43:46 +0200

    Add some new functions and remove some sintax errors.

--- setup.py
+++ setup.py
@@ -24,30 +24,41 @@ def print_file(path):
 			print(line, end='')
 	file.close()
 
-def make_file_backup(path):
-	shutil.copy2(path, path + BACKUP_EXTENSION)
+def get_backup_path(path):
+	return path + BACKUP_EXTENSION
 
-def replace_in_file(file, old_string, new_string)
+def make_file_backup(path):
+	shutil.copy2(path, get_backup_path(path))
+
+def recover_file_backup(path):
+	shutil.copy2(get_backup_path(path), path)
+
+def replace_in_file(file, old_string, new_string):
 	replaced = False
 	for line in file:
-		if(line == old_string)
+		if(line == old_string):
 			file.write(new_string)
 			replaced = True
 		else:
 			file.write(line)
 	return value
 	
-def configurate_network_interface():
-	if path.exists(PATH_NET_INTERFACE)
-		try:
-			if !ARGS.no-backup:
-				make_file_backup(PATH_NET_INTERFACES)
-			file = open(PATH_NET_INTERFACES, mode='+')
-			if !replace_in_file(file, 'iface eth0 inet manual', 'iface eth0 inet static\n\taddress 172.24.1.1\n\tnetmask 255.255.255.0\n\tnetwork 172.24.1.0\n\tbroadcst 172.24.1.255\n'):
-				raise Error('The file {0} has been modificated before.'.format(PATH_NET_INTERFACES))
-		except OSError as error:
-			file.close()
-			print('OS Error: {0}'.format(error))
-	if !ARGS.no-backup:
-		
+def configurate_network_interfaces():
+	if path.exists(PATH_NET_INTERFACES) == False:
+		raise Error('The file {0} do not exists.'.format(PATH_NET_INTERFACES))
+	try:
+		if ARGS.no-backup == False:
+			make_file_backup(PATH_NET_INTERFACES)
+		file = open(PATH_NET_INTERFACES, mode='+')
+		if replace_in_file(file, 'iface eth0 inet manual', 'iface eth0 inet static\n\taddress 172.24.1.1\n\tnetmask 255.255.255.0\n\tnetwork 172.24.1.0\n\tbroadcst 172.24.1.255\n') == False:
+			raise Error('The file {0} has been modificated before.'.format(PATH_NET_INTERFACES))
+	except OSError as error:
+		file.close()
+		raise error
+	
+def recover_network_interfaces():
+	path_backup = get_backup_path(PATH_NET_INTERFACES)
+	if path.exists(path_backup) == False:
+		raise Error('Cannot recover {0} to this original state, {1} does not exists.'.format(PATH_NET_INTERFACES, path_backup))
+	recover_file_backup(PATH_NET_INTERFACES)
 
