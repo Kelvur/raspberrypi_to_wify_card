8a9ceb76cd04dc523b0f8196122663c83d5df0b7
Kelvur <Osorio10.92@gmail.com>
2017-08-10 14:19:58 +0200

    Tests driven and fix bugs in configure_network part.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def parse_args():
 	return parser.parse_args()
 
 ARGS = parse_args()
-PATH_NET_INTERFACES = '/etc/network/interfaces'
+PATH_NET_INTERFACES = '/etc/network/interfaces.test'
 PATH_DHCPCD_CONF = '/etc/dhcpcd.conf'
 PATH_DNSMASQ_CONF = '/etc/dnsmasp.conf'
 BACKUP_EXTENSION = '.backup'
@@ -33,32 +33,36 @@ def make_file_backup(path):
 def recover_file_backup(path):
 	shutil.copy2(get_backup_path(path), path)
 
-def replace_in_file(file, old_string, new_string):
+def replace_in_file(file_name, old_string, new_string):
 	replaced = False
-	for line in file:
-		if(line == old_string):
-			file.write(new_string)
-			replaced = True
-		else:
-			file.write(line)
-	return value
+	buffer = ''
+	with open(file_name, mode='r') as file:
+		for line in file:
+			if(line == old_string):
+				buffer += new_string
+				replaced = True
+			else:
+				buffer += line
+	with open(file_name, mode='w') as file:
+		print(buffer, file=file)
+	return replaced
 	
 def configurate_network_interfaces():
-	if path.exists(PATH_NET_INTERFACES) == False:
-		raise Error('The file {0} do not exists.'.format(PATH_NET_INTERFACES))
-	try:
-		if ARGS.no-backup == False:
-			make_file_backup(PATH_NET_INTERFACES)
-		file = open(PATH_NET_INTERFACES, mode='+')
-		if replace_in_file(file, 'iface eth0 inet manual', 'iface eth0 inet static\n\taddress 172.24.1.1\n\tnetmask 255.255.255.0\n\tnetwork 172.24.1.0\n\tbroadcst 172.24.1.255\n') == False:
-			raise Error('The file {0} has been modificated before.'.format(PATH_NET_INTERFACES))
-	except OSError as error:
-		file.close()
-		raise error
+	if os.path.exists(PATH_NET_INTERFACES) == False:
+		raise IOError('The file {0} do not exists.'.format(PATH_NET_INTERFACES))
+	if ARGS.no_backup == False:
+		backup_network_interfaces()
+	if replace_in_file(PATH_NET_INTERFACES, 'iface eth0 inet manual\n', 'iface eth0 inet static\n\taddress 172.24.1.1\n\tnetmask 255.255.255.0\n\tnetwork 172.24.1.0\n\tbroadcast 172.24.1.255\n') == False:
+		raise IOError('The file {0} has been modificated before.'.format(PATH_NET_INTERFACES))
+
+
+def backup_network_interfaces():
+	make_file_backup(PATH_NET_INTERFACES)
 	
 def recover_network_interfaces():
 	path_backup = get_backup_path(PATH_NET_INTERFACES)
-	if path.exists(path_backup) == False:
-		raise Error('Cannot recover {0} to this original state, {1} does not exists.'.format(PATH_NET_INTERFACES, path_backup))
+	if os.path.exists(path_backup) == False:
+		raise IOError('Cannot recover {0} to this original state, {1} does not exists.'.format(PATH_NET_INTERFACES, path_backup))
 	recover_file_backup(PATH_NET_INTERFACES)
 
+configurate_network_interfaces()
