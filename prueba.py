32fce4918506234d544cde17be1b9eccb382db78
Kelvur <Osorio10.92@gmail.com>
2017-09-19 20:53:58 +0200

    Refactor of prueba.py, there is more work to do.

--- prueba.py
+++ prueba.py
@@ -4,22 +4,7 @@ import shutil
 import subprocess
 import argparse
 import os.path
-
-class BaseError(Exception):
-	"""Base error class for this module"""
-	pass
-
-class BackupError(BaseError):
-	""""""
-	pass
-
-class PermisionError(BaseError):
-	""""""
-	pass
-
-class FileError(BaseError):
-	""""""
-	pass
+import re
 
 class BasicConfigurationModel:
 	
@@ -29,38 +14,105 @@ class BasicConfigurationModel:
 	path = ''
 	backup_path = ''
 	test_path = ''
+	settings = {}
 
-	def __init__(self, backup=true):
-		backup_path = get_backup_path(path)
-		test_path = get_test_path(path)
-		configurate(self, path)
+	def __init__(self, backup=True):
+		self.backup_path = self.get_backup_path(self.path)
+		self.test_path = self.get_test_path(self.path)
+		self.settings['backup'] = backup
 
-	@classmethod
+	@staticmethod
 	def get_backup_path(path):
-		return path + BACKUP_EXTENSION
+		return path + BasicConfigurationModel.BACKUP_EXTENSION
 
-	@classmethod
+	@staticmethod
 	def get_test_path(path):
-		return path + TEST_EXTENSION
+		return path + BasicConfigurationModel.TEST_EXTENSION
 
-	@classmethod
+	@staticmethod
 	def exists(path):
 		return os.path.exists(path)
+	
+	@staticmethod
+	def copy(origin, destiny):
+		try:
+			shutil.copy2(origin, destiny)
+		except Error:
+			return False
+		return True
 
-	def configurate(self, path):
-		return
+	@staticmethod
+	def remove(path):
+		try:
+			subprocess.check_call(['rm', path])
+		except CalledProcessError:
+			return False
+		return True
 
-	#def copy(origin, destiny):
+	@staticmethod
+	def replace_in_file(path, old_string, new_string):
+		replace = False
+		buffer = ''
+		with open(path, mode='r') as file:
+			for line in file:
+				if line == oldstring:
+					buffer += new_string
+					replaced = True
+				else:
+					buffer += line
+		with open(path, mode='w') as file:
+			print(buffer, file=file)
+		return replace
+
+	@staticmethod
+	def read_file(path):
+		buffer = ''
+		with open(path, mode='r') as file:
+			for line in file:
+				buffer += line
+		return buffer
+
+	@staticmethod
+	def write_file(path, buffer):
+		with open(path, mode='w') as file:
+			print(buffer, file=file)
+
+	def configure(self):
+		if self.settings['backup']:
+			self.backup()
+		if self._configure() == False:
+			if self.settings['backup']:
+				self.restore_backup()
+
+	def _configure(self):
+		return False
 
 	def backup(self):
-		shutil.copy2(self.path, self.backup_path)
+		return self.copy(self.path, self.backup_path)
+
+	def remove_backup(self):
+		self.remove(self.backup_path)
 
 	def restore_backup(self):
-		if exists(self.backup_path):
-			shutil.copy2(self.backup_path, self.path)
-		else:
-			raise FileError()
+		return self.copy(self.backup_path, self.path)	
+
+
+class ConfigurationNetworkInterfaces(BasicConfigurationModel):
 	
-	def remove_backup(self):
-		if exists(self.backup_path):
-			break
+	path = '/home/pi/test/test.py'
+	ETH0_INTERFACE = 'iface eth0 inet static\n\taddress 172.24.1.1\n\tnetmask 255.255.255.0\n\tnetwork 172.24.1.0\n\tbroadcast 172.24.1.255\n'
+	ETH0_REGEX = r'^iface eth0 inet (manual)|(static(\n\t[\w\.\-/])+)'
+
+	def __init__(self):
+		BasicConfigurationModel.__init__(self)
+	
+	def _configure(self):
+		buffer = self.read_file(self.path)
+		result = re.subn(self.ETH0_REGEX, self.ETH0_INTERFACE, buffer, flags=re.MULTILINE)
+		if result[1] == 0:
+			return False
+		self.write_file(self.path, result[0])
+		return True
+
+conf = ConfigurationNetworkInterfaces()
+conf.configure()
